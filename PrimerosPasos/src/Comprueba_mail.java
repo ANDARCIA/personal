2bb07498dfdb5a2a72be216c14f6da50336dc2f3
ANDARCIA <eduardojiaozho@gmaill.com>
2018-03-01 18:19:01 -0400

    Guardando cambiios

---
+++ PrimerosPasos/src/Comprueba_mail.java
@@ -0,0 +1,48 @@
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ */
+
+/**
+ * @author jiaozhi
+ *
+ */
+public class Comprueba_mail {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean punto=false;
+		int arroba=0;
+        
+        String mail=JOptionPane.showInputDialog("Introduzca mail");
+        
+        for (int i = 0; i < mail.length(); i++) {
+            
+            if (mail.charAt(i)=='@') {
+                
+                arroba++;
+            }
+            
+            if (mail.charAt(i)=='.') {
+            	
+            	punto=true;
+            	
+            }
+            if (arroba==1 && punto==true) {
+                
+                System.out.println("Es correcto");
+                
+            } else {
+                
+                System.out.println("No es correcto");
+            }
+            
+        }
+	}
+
+}
