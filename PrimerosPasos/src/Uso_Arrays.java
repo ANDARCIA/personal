255c09daa0e15e7e19ae80e33ac59a36d002d812
ANDARCIA <eduardojiaozho@gmail.com>
2018-03-01 23:22:35 -0400

    arrays i

---
+++ PrimerosPasos/src/Uso_Arrays.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author jiaozhi
+ *
+ */
+public class Uso_Arrays {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] mi_matriz={5,38,-15,92,71};
+		
+		System.out.println(mi_matriz[3]);
+		
+	}
+
+}
