5abb5c76811c2f9cfe45e21fdaab5515b8e3681a
ANDARCIA <eduardojiaozho@gmail.com>
2018-04-27 14:45:16 -0400

    Guardando cambios

--- src/primerospasos/Inicio.java
+++ src/primerospasos/Inicio.java
@@ -17,6 +17,7 @@ public class Inicio {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Bienvenido a las Clases de Java");
+        System.out.println("Prueba de git");
     }
     
 }
